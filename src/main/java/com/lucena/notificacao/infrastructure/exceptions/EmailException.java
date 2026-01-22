c47790c3166b733dba7e256752f2e75e9a6ed83e
joselucenaf <joselucenafn@gmail.com>
2026-01-22 02:30:22 -0300

    Adicionados métodos de envio de email

---
+++ src/main/java/com/lucena/notificacao/infrastructure/exceptions/EmailException.java
@@ -0,0 +1,10 @@
+package com.lucena.notificacao.infrastructure.exceptions;
+
+public class EmailException extends RuntimeException{
+    public EmailException(String mensagem){
+        super(mensagem);
+    }
+    public EmailException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
